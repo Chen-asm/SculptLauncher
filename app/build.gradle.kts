4f90512db1c2b2f7e189ff5e85cd024ac46d0f2c
Yuanshine <demwre@outlook.com>
2024-08-25 20:23:17 +0800

    更正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.preference)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,15 +1,10 @@
 #include <jni.h>
 #include <string>
-#include <jni.h>
-#include <jni.h>
-#include <filesystem>
-#include <fstream>
-
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_chen_sculptlauncher_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    string hello = "Hello from C++";
+    std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
